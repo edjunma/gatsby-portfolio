73a7559a5fc19de0dcf3ee0e8f8f0a677b016957
edjunma <edjunma@gmail.com>
2020-01-08 04:13:54 -0500

    working on navbar, update files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `EJM Portfolio`,
+    description: `My Gatsby Portfolio, Enjoy (:`,
+    author: `@edjunma`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,7 +1,22 @@
 import React from "react"
+import PropTypes from "prop-types"
+import styled from "styled-components"
+import { animated } from "react-spring"
+
+const StyledHeader = styled(animated.header)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  max-width: 100vw;
+  background: var(--navbar);
+  box-shadow: 0 0.5rem 2rem #333;
+  transition: background 0.2s ease-out;
+  z-index: 100;
+`
 
 const Navbar = () => {
-  return <header></header>
+  return <StyledHeader></StyledHeader>
 }
 
 export default Navbar

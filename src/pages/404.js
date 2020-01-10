8239d45f291da7d9c87d2280207ca19348cfe7ea
edjunma <edjunma@gmail.com>
2020-01-10 01:08:00 -0500

    add minimal code to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,14 @@
 import React from "react"
+import styled from 'styled-components';
+import { Link } from 'gatsby';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const StyledLink = styled(Link)`
+  background: transparent;
+  outline: none;
+`
 
 const NotFoundPage = () => (
   <Layout>
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,16 +1,16 @@
 import React from "react"
-import { Link } from "gatsby"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const AboutPage = () => (
-  <Layout>
-    <SEO title="About Page" />
-    <h1>Hi from the About Page</h1>
-    <p>Welcome to About Page</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+  <div>
+    <h1>About Us</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
+      adipisci, voluptatum, nisi velit praesentium error quia explicabo voluptas
+      ad recusandae enim. Recusandae sequi non vel voluptas. Labore omnis
+      provident ex, perferendis esse, voluptate atque alias officiis dolor
+      eligendi itaque qui?
+    </p>
+  </div>
 )
 
 export default AboutPage

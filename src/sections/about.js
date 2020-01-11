400e52ce6db00241e0bc371c0e5ddd4dce07535a
edjunma <edjunma@gmail.com>
2020-01-11 03:03:14 -0500

    starting on about section

---
+++ src/sections/about.js
@@ -0,0 +1,22 @@
+import React from "react"
+import styled from "styled-components"
+
+import {
+  Contained,
+  StyledSection,
+  Wrapper,
+} from "../components/layout/elements"
+
+const About = () => {
+
+
+  return (
+    <StyledSection>
+      <Contained>
+        <Wrapper></Wrapper>
+      </Contained>
+    </StyledSection>
+  )
+}
+
+export default About

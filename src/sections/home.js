d9c24db8e6a35382420a75db98b33bb9e885820f
edjunma <edjunma@gmail.com>
2020-01-11 02:50:51 -0500

    setup home page

--- src/sections/home.js
+++ src/sections/home.js
@@ -7,11 +7,31 @@ import {
   Wrapper,
 } from "../components/layout/elements"
 
+const SmallWrapper = styled.div`
+  width: 90%;
+  margin: 0 auto;
+`
+
+const Title = styled(animated.h1)`
+  font-size: 6rem;
+  font-weight: 300;
+  margin-bottom: 2rem;
+  margin: 0;
+  line-height: 1.3;
+  color: var(--text-highlight);
+  transition: color 0.2s ease-out;
+`
+
+
 const Home = () => {
   return (
     <StyledSection>
       <Contained>
-        <Wrapper></Wrapper>
+        <Wrapper>
+          <SmallWrapper>
+            <Title></Title>
+          </SmallWrapper>
+        </Wrapper>
       </Contained>
     </StyledSection>
   )
